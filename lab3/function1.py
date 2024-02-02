22c759bff2133eb5d5982abe7719eee61d82951d
dayanaamanzhol <156893198+dayanaamanzhol@users.noreply.github.com>
2024-02-02 22:47:25 +0600

    Update function1.py

--- lab3/function1.py
+++ lab3/function1.py
@@ -128,3 +128,27 @@ def histogram(nums):
 numbers = input()
 numbers = numbers.split()
 histogram(numbers)
+
+#ex13
+import random
+def play():
+    print("Hello! What is your name?")
+    name = input()
+    print(f"Well, {name},I am thinking of a number between 1 and 20.")
+
+    number = random.randint(1, 20)
+    guesses = 0
+    while True:
+        print("Take a guess.")
+        guess = int(input())
+        guesses += 1
+        if guess < number:
+            print("Your guess is too low.")
+        elif guess > number:
+            print("Your guess is too high.")
+        else:
+            break
+
+    print(f"Good job, {name}! You guessed my number in {guesses} guesses!")
+    
+play()
