8b609af268a18a5040d6153317f4f9e70aa98337
dayanaamanzhol <156893198+dayanaamanzhol@users.noreply.github.com>
2024-03-04 22:04:02 +0500

    Update dicandfun.py

--- lab6/dicandfun.py
+++ lab6/dicandfun.py
@@ -1,3 +1,63 @@
+#ex3
+import os
+
+def path_info(path):
+    if os.path.exists(path):
+        filename = os.path.basename(path)
+        directory = os.path.dirname(path)
+        return True, filename, directory
+    else:
+        return False, None, None
+
+path = "/User/qwerty/Desktop/file.txt"
+
+exists, filename, directory = path_info(path)
+
+if exists:
+    print(f"Path exists")
+    print(f"Filename: {filename}")
+    print(f"Directory: {directory}")
+else:
+    print(f"Path does not exist")
+
+#ex4
+with open('text.txt', 'r') as f:
+    count = 0
+    for line in f:
+        count += 1
+    print(count)
+    
+#ex5
+list = [1, 2, 3, 4, 5]
+with open('text.txt', 'w') as f:
+    for i in list:
+        f.write(str(i) + ' ')
+
+#ex6
+import string
+
+def generate():
+    for letter in string.ascii_uppercase:
+        with open(f'{letter}.txt', 'w') as f:
+            pass
+
+generate()
+
+#ex7
+with open('text.txt', 'r') as rf:
+    with open('text-copy.txt', 'w') as wf:
+        for line in rf:
+            wf.write(line)
+
+#ex8
+import os
+path = "/User/qwerty/Desktop/file.txt"
+
+if os.path.exists(path):
+  os.remove(path)
+else:
+  print("The file does not exist")
+    
 #ex1
 import os
 
@@ -18,7 +78,7 @@ def list_directories_and_files(path):
             files.append(item)
     return directories, files
 
-path = "////"
+path = "/User/qwerty/Desktop/file.txt"
 
 print("Directories:")
 print(list_directories(path))
@@ -45,7 +105,7 @@ def check_access(path):
     }
     return access_info
 
-path = "////"
+path = "/User/qwerty/Desktop/file.txt"
 
 access_info = check_access(path)
 
@@ -55,90 +115,3 @@ print(f"Readable: {access_info['readable']}")
 print(f"Writable: {access_info['writable']}")
 print(f"Executable: {access_info['executable']}")
 
-#ex3
-import os
-
-def path_info(path):
-    if os.path.exists(path):
-        filename = os.path.basename(path)
-        directory = os.path.dirname(path)
-        return True, filename, directory
-    else:
-        return False, None, None
-
-path = "////f"
-
-exists, filename, directory = path_info(path)
-
-if exists:
-    print(f"Path exists")
-    print(f"Filename: {filename}")
-    print(f"Directory: {directory}")
-else:
-    print(f"Path does not exist")
-
-#ex4
-def count(file):
-    with open(filename, 'r') as file:
-        return sum(1 for line in file)
-
-file = "///file.txt"
-
-line_count = count(file)
-print(line_count)
-
-#ex5
-def write_list_to_file(file, lst):
-    with open(filename, 'w') as file:
-        for item in lst:
-            file.write(str(item) + '\n')
-
-file = "///file.txt"
-
-lst = [1, 2, 3, 4, 5]
-
-write_list_to_file(file, lst)
-
-print(file)
-
-#ex6
-import string
-
-def generate():
-    for letter in string.ascii_uppercase:
-        file = f"{letter}.txt"
-        with open(file, 'w') as file:
-            file.write(f"This is file {file}\n")
-        print(f"File {file} created.")
-
-generate()
-
-#ex7
-def copy_file(source_file, destination_file):
-    with open(source_file, 'r') as source, open(destination_file, 'w') as destination:
-        for line in source:
-            destination.write(line)
-
-source_file = "a.txt"
-destination_file = "b.txt"
-
-copy_file(source_file, destination_file)
-
-print(f"Contents of {source_file} copied to {destination_file}")
-
-#ex8
-import os
-
-def delete_file(path):
-    if os.path.exists(path):
-        if os.access(path, os.W_OK):
-            os.remove(path)
-            print(f"File {path} deleted successfully")
-        else:
-            print(f"Permission denied: {path}")
-    else:
-        print(f"File not found: {path}")
-
-path = "///file.txt"
-
-delete_file(path)
