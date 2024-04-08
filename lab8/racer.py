14ab6aeabed5aaa7b2d0f2cbf67be3cc2a203638
dayanaamanzhol <156893198+dayanaamanzhol@users.noreply.github.com>
2024-04-08 22:18:30 +0500

    Update racer.py

--- lab8/racer.py
+++ lab8/racer.py
@@ -1,197 +1,141 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
-import random
+import random, time
 
 pygame.init()
 
-width = 500
-height = 500
-screen_size = (width, height)
-screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption('Car Game')
+FPS = 60
+FramePerSec = pygame.time.Clock()
 
-gray = (100, 100, 100)
-green = (76, 208, 56)
-red = (200, 0, 0)
+blue  = (0, 0, 255)
+red   = (255, 0, 0)
+green = (0, 255, 0)
+black = (0, 0, 0)
 white = (255, 255, 255)
-yellow = (255, 232, 0)
 
-road_width = 300
-marker_width = 10
-marker_height = 50
-
-left_lane = 150
-center_lane = 250
-right_lane = 350
-lanes = [left_lane, center_lane, right_lane]
-
-road = (100, 0, road_width, height)
-left_edge_marker = (95, 0, marker_width, height)
-right_edge_marker = (395, 0, marker_width, height)
-
-lane_marker_move_y = 0
-
-player_x = 250
-player_y = 400
-
-clock = pygame.time.Clock()
-fps = 120
-
-gameover = False
-speed = 2
+w = 400
+h = 600
+speed = 5
 score = 0
+coins_collected = 0
 
-class Vehicle(pygame.sprite.Sprite):
-    
-    def __init__(self, image, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        
-        image_scale = 45 / image.get_rect().width
-        new_width = image.get_rect().width * image_scale
-        new_height = image.get_rect().height * image_scale
-        self.image = pygame.transform.scale(image, (new_width, new_height))
-        
+
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, black)
+
+background = pygame.image.load("PygameTutorial_3_0/AnimatedStreet.png")
+
+screen = pygame.display.set_mode((w,h))
+pygame.display.set_caption("racer")
+
+class Enemy(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("PygameTutorial_3_0/Enemy.png")
         self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
-        
-class PlayerVehicle(Vehicle):
-    
-    def __init__(self, x, y):
-        image = pygame.image.load('images/car.png')
-        super().__init__(image, x, y)
-        
-player_group = pygame.sprite.Group()
-vehicle_group = pygame.sprite.Group()
+        self.rect.center = (random.randint(40, w-40), 0)  
 
-player = PlayerVehicle(player_x, player_y)
-player_group.add(player)
-
-image_filenames = ['pickup_truck.png', 'semi_trailer.png', 'taxi.png', 'van.png']
-vehicle_images = []
-for image_filename in image_filenames:
-    image = pygame.image.load('images/' + image_filename)
-    vehicle_images.append(image)
-    
-crash = pygame.image.load('images/crash.png')
-crash_rect = crash.get_rect()
-
-running = True
-while running:
-    
-    clock.tick(fps)
-    
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-            
-        if event.type == KEYDOWN:
-            
-            if event.key == K_LEFT and player.rect.center[0] > left_lane:
-                player.rect.x -= 100
-            elif event.key == K_RIGHT and player.rect.center[0] < right_lane:
-                player.rect.x += 100
-                
-            for vehicle in vehicle_group:
-                if pygame.sprite.collide_rect(player, vehicle):
-                    
-                    gameover = True
-                    
-                    if event.key == K_LEFT:
-                        player.rect.left = vehicle.rect.right
-                        crash_rect.center = [player.rect.left, (player.rect.center[1] + vehicle.rect.center[1]) / 2]
-                    elif event.key == K_RIGHT:
-                        player.rect.right = vehicle.rect.left
-                        crash_rect.center = [player.rect.right, (player.rect.center[1] + vehicle.rect.center[1]) / 2]
-            
-            
-    screen.fill(green)
-    
-    
-    pygame.draw.rect(screen, gray, road)
-    
-    pygame.draw.rect(screen, yellow, left_edge_marker)
-    pygame.draw.rect(screen, yellow, right_edge_marker)
-    
-    lane_marker_move_y += speed * 2
-    if lane_marker_move_y >= marker_height * 2:
-        lane_marker_move_y = 0
-    for y in range(marker_height * -2, height, marker_height * 2):
-        pygame.draw.rect(screen, white, (left_lane + 45, y + lane_marker_move_y, marker_width, marker_height))
-        pygame.draw.rect(screen, white, (center_lane + 45, y + lane_marker_move_y, marker_width, marker_height))
-        
-    player_group.draw(screen)
-    
-    if len(vehicle_group) < 2:
-        
-        add_vehicle = True
-        for vehicle in vehicle_group:
-            if vehicle.rect.top < vehicle.rect.height * 1.5:
-                add_vehicle = False
-                
-        if add_vehicle:
-            
-            lane = random.choice(lanes)
-            
-            image = random.choice(vehicle_images)
-            vehicle = Vehicle(image, lane, height / -2)
-            vehicle_group.add(vehicle)
-    
-    for vehicle in vehicle_group:
-        vehicle.rect.y += speed
-        
-        if vehicle.rect.top >= height:
-            vehicle.kill()
-            
+      def move(self):
+        global score
+        self.rect.move_ip(0,speed)
+        if (self.rect.top > 600):
+            global score
             score += 1
-            
-            if score > 0 and score % 5 == 0:
-                speed += 1
-    
-    vehicle_group.draw(screen)
-    
-    font = pygame.font.Font(pygame.font.get_default_font(), 16)
-    text = font.render('Score: ' + str(score), True, white)
-    text_rect = text.get_rect()
-    text_rect.center = (50, 400)
-    screen.blit(text, text_rect)
-    
-    if pygame.sprite.spritecollide(player, vehicle_group, True):
-        gameover = True
-        crash_rect.center = [player.rect.center[0], player.rect.top]
-            
-    if gameover:
-        screen.blit(crash, crash_rect)
-        
-        pygame.draw.rect(screen, red, (0, 50, width, 100))
-        
-        font = pygame.font.Font(pygame.font.get_default_font(), 16)
-        text = font.render('Game over. Play again? (Enter Y or N)', True, white)
-        text_rect = text.get_rect()
-        text_rect.center = (width / 2, 100)
-        screen.blit(text, text_rect)
-            
-    pygame.display.update()
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, w - 40), 0)
 
-    while gameover:
-        
-        clock.tick(fps)
-        
-        for event in pygame.event.get():
-            
-            if event.type == QUIT:
-                gameover = False
-                running = False
-                
-            if event.type == KEYDOWN:
-                if event.key == K_y:
-                    gameover = False
-                    speed = 2
-                    score = 0
-                    vehicle_group.empty()
-                    player.rect.center = [player_x, player_y]
-                elif event.key == K_n:
-                    # exit the loops
-                    gameover = False
-                    running = False
+
+class Coin(pygame.sprite.Sprite):
+  def __init__(self):
+      super().__init__()
+      self.image = pygame.image.load("PygameTutorial_3_0/coin.png")
+      self.rect = self.image.get_rect()
+      self.rect.center = (random.randint(40, w - 40), 0)
+
+  def move(self):
+      global coins_collected
+      self.rect.move_ip(0, speed)
+      if self.rect.top > h:
+          self.rect.top = 0
+          self.rect.center = (random.randint(40, w - 40), 0)
+
+      # Check for collision with player
+      if self.rect.colliderect(P1.rect):
+          coins_collected += 1
+          self.rect.top = 0
+          self.rect.center = (random.randint(40, w - 40), 0)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("PygameTutorial_3_0/Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+    def move(self):
+      pressed_keys = pygame.key.get_pressed()
+
+      if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+      if self.rect.right < w:        
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+#Setting up Sprites        
+P1 = Player()
+E1 = Enemy()
+coin = Coin()
+
+
+#Creating Sprites Groups
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+coins = pygame.sprite.Group()
+all_sprites.add(coin)
+
+
+#Adding a new User event 
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+#Game Loop
+while True:
+
+    #Cycles through all events occurring  
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+              speed += 0.5     
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.blit(background, (0,0))
+    scores = font_small.render(f"Score:{score}", True, black)
+    coin_text = font_small.render(f"Coins: {coins_collected}", True, black)
+    screen.blit(scores, (10,10))
+    screen.blit(coin_text, (w - 100, 10))
+
+
+    #Moves and Re-draws all Sprites
+    for entity in all_sprites:
+        screen.blit(entity.image, entity.rect)
+        entity.move()
+
+    #To be run if collision occurs between Player and Enemy
+    if pygame.sprite.spritecollideany(P1, enemies):
+          pygame.mixer.Sound('PygameTutorial_3_0/crash.wav').play()
+          time.sleep(0.5)
+
+          screen.fill(red)
+          screen.blit(game_over, (30,250))
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
+
 
 pygame.quit()
